c0c45c8d263f62353eae1bf1b23189abbc405315
Stefano Sinatti <stefano.sinatti@regione.emilia-romagna.it>
2023-09-21 16:33:00 +0200

    Release 0.0.11 (#1)
    * Update for release 0.0.11
    
    * Update pom.xml

--- SQL/registra_evento_utente.sql
+++ SQL/registra_evento_utente.sql
@@ -2,9 +2,13 @@ create or replace PROCEDURE registra_evento_utente (
 /*
  * Registra la riga relativa all'evento di login, errore di login 
  * o reset/cambio password, eliminando se necessario le vecchie registrazioni
- * di LOGIN_OK per le connessioni da ws (sono troppe ed inutili)
+ * per le connessioni da ws (sono troppe ed inutili).
  *
  * author: ff
+ * note:
+ * 2018-02-01 aggiunto il lock esclusivo prima delle operazioni di delete
+ *            per evitare problemi di concorrenza/deadlock con le chiamate
+ *            provenienti da ws
  * ex:
  begin 
     REGISTRA_EVENTO_UTENTE(
@@ -36,12 +40,122 @@ create or replace PROCEDURE registra_evento_utente (
     p_ds_evento       IN VARCHAR2, -- la descrizione dell'evento
     p_dt_evento       IN TIMESTAMP -- il timestamp dell'evento da scrivere
 ) IS
-    tmp_conta   NUMBER;
+    tmp_conta       NUMBER;
+    tmp_is_automa   NUMBER;
+    tmp_usr_user    usr_user%rowtype;
 BEGIN
 -- Gli eventi da registrare sono:
--- 'BAD_PASS', 'BAD_USER', 'EXPIRED', 'LOCKED' errori, da conservare
--- 'LOGIN_OK', positivo, vanno eliminati i dati vecchi per gli automi 
+-- 'BAD_PASS', 'BAD_USER', 'EXPIRED', 'LOCKED' errori
+-- 'LOGIN_OK', positivo
+--  di questi eventi vanno eliminati i dati vecchi per gli automi 
 -- 'SET_PSW', neutro, va conservato
+
+
+
+-- verifico se è un automa
+    SELECT CASE
+            WHEN EXISTS (
+                SELECT NULL
+                FROM usr_user t
+                WHERE t.nm_userid = p_nm_userid
+                    AND t.tipo_user = 'AUTOMA'
+            ) THEN 1
+            ELSE 0
+        END
+    INTO
+        tmp_is_automa
+    FROM dual;
+
+    --   dbms_output.put_line('automa? '||tmp_is_automa);
+
+-- verifico se oltre ad essere un automa, sta registrando un LOGIN_OK
+
+    IF
+        tmp_is_automa = 1 AND p_tipo_evento = 'LOGIN_OK'
+    THEN
+    -- verifico se ci sono registrazioni
+    -- più vecchie delle ore 00 di oggi
+    -- e/o del timestamp della riga che si vuole inserire,
+    -- per evitare incoerenze nel caso la data
+    -- sia nel passato; la cosa è possibile
+    -- se i clock del appserver e del db server sono
+    -- desincronizzati e questa stored viene chiamata alle 23.59
+    --
+    -- questo controllo riduce il rischio di avere lock sulla
+    -- tabella a causa della delete se non è strettamente necessario
+    --
+    --   dbms_output.put_line('LOGIN_OK ed automa');
+    --
+        SELECT CASE
+                WHEN EXISTS (
+                    SELECT NULL
+                    FROM sacer_log.log_evento_login_user t
+                    WHERE t.dt_evento < trunc(SYSDATE)
+                        AND t.dt_evento < p_dt_evento
+                        AND t.nm_userid = p_nm_userid
+                        AND t.tipo_evento IN (
+                                'LOGIN_OK', 'BAD_PASS', 'BAD_USER', 'EXPIRED', 'LOCKED'
+                            )
+                ) THEN 1
+                ELSE 0
+            END
+        INTO
+            tmp_conta
+        FROM dual;
+        
+        --   dbms_output.put_line('vecchi log tmp_conta? '||tmp_conta);
+
+        IF
+            tmp_conta > 0
+        THEN
+        -- se ci sono le elimino:
+        -- 
+        -- blocco il record di usr_user, devo impedire che questa
+        -- procedura possa essere eseguita da più client simultaneamente
+        -- (questo è normale in un contesto WS)
+            SELECT *
+                INTO
+                    tmp_usr_user
+                FROM usr_user t
+                WHERE t.nm_userid = p_nm_userid
+            FOR UPDATE WAIT 35;
+        -- cancello i record
+            DELETE FROM sacer_log.log_evento_login_user t WHERE t.dt_evento < trunc(SYSDATE)
+                AND t.dt_evento < p_dt_evento
+                AND t.nm_userid = p_nm_userid
+                AND t.tipo_evento IN (
+                        'LOGIN_OK', 'BAD_PASS', 'BAD_USER', 'EXPIRED', 'LOCKED'
+                    );
+
+        END IF;
+
+    END IF;
+    
+-- verifico se è SET_PSW ed è un automa
+
+    IF
+        tmp_is_automa = 1 AND p_tipo_evento = 'SET_PSW'
+    THEN
+    
+    --   dbms_output.put_line('SET_PSW ed automa');
+    
+    -- elimino tutte le registrazioni del ws precedenti il
+    -- timestamp della riga da inserire.
+    -- in questo caso non dovrei avere problemi di concorrenza poiché
+    -- questa funzione è attivabile solo online e non da ws.
+    --
+    -- cancello i record
+        DELETE FROM sacer_log.log_evento_login_user t WHERE t.dt_evento < p_dt_evento
+            AND t.nm_userid = p_nm_userid
+            AND t.tipo_evento IN (
+                    'LOGIN_OK', 'BAD_PASS', 'BAD_USER', 'EXPIRED', 'LOCKED'
+                );
+
+    END IF;
+    
+
+-- registro l'evento, finalmente
+
     INSERT INTO sacer_log.log_evento_login_user (
         id_evento_login_user,
         nm_userid,
@@ -59,55 +173,7 @@ BEGIN
         p_tipo_evento,
         p_ds_evento
     );
-
-    -- dbms_output.put_line('scritto riga');
-
--- verifico se è LOGIN_OK ed è un automa
-
-    SELECT COUNT(*)
-    INTO
-        tmp_conta
-    FROM usr_user t
-    WHERE t.nm_userid = p_nm_userid
-        AND t.tipo_user = 'AUTOMA';
-
-    IF
-        tmp_conta = 1 AND p_tipo_evento = 'LOGIN_OK'
-    THEN
-    -- conto se ci sono analoghe registrazioni
-    -- più vecchie delle ore 00 di oggi
-    -- e/o della data della riga che si vuole inserire,
-    -- per evitare di cancellare quanto appena inserito
-    -- nel caso la data sia nel passato, cosa possibile
-    -- se i clock del appserver e del db server sono
-    -- desincronizzati e questa stored viene chiamata alle 23.59
-        -- dbms_output.put_line('LOGIN_OK ed automa');
-        --
-        SELECT COUNT(*)
-        INTO
-            tmp_conta
-        FROM sacer_log.log_evento_login_user t
-        WHERE t.dt_evento < trunc(SYSDATE)
-            AND t.dt_evento < p_dt_evento
-            AND t.nm_userid = p_nm_userid
-            AND t.tipo_evento = 'LOGIN_OK';
-
-        IF
-            tmp_conta > 0
-        THEN
-        -- dbms_output.put_line('tmp_conta ' || tmp_conta);
-        -- se ci sono le elimino.
-        -- qusto controllo riduce il rischio di inserire un lock sulla
-        -- tabella a causa della delete se non è strettamente necessario
-            DELETE FROM sacer_log.log_evento_login_user t WHERE t.dt_evento < trunc(SYSDATE)
-                AND t.dt_evento < p_dt_evento
-                AND t.nm_userid = p_nm_userid
-                AND t.tipo_evento = 'LOGIN_OK';
-
-        END IF;
-
-    END IF;
-
-    -- dbms_output.put_line('finito');
+    
+    --   dbms_output.put_line('scritto la riga');
 
 END;
